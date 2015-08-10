6028cdcb5c5c7bbf5e9421d8785f49865ddd681e
txhc4life <txhc4life@gmail.com>
2015-08-10 15:12:44 -0500

    updated code from last night

--- raximg.py
+++ raximg.py
@@ -1,5 +1,6 @@
 import pyrax 
 import time
+import os
 
 pyrax.set_setting("identity_type", "rackspace")
 
@@ -32,42 +33,14 @@ def export_img():
        time.sleep(60)
        task.reload()
        print task.status
-       
 
-def download_vhd():
-    #Function to download image
-    username = raw_input('What is your username? ')
-    apiKey = raw_input('What is your api key? ')
-    region = raw_input('What region is the image in? ')
-   
-    pyrax.set_credentials(username, apiKey, region= region)
+    vhd = imageID + ".vhd"   
+     
 
-    pyrax.cloudfiles.download_object(region, file ,directory)
+    path = os.getcwd()
+    # Download file   
+    pyrax.cloudfiles.download_object(container, vhd, path)
 
-def upload_vhd():
-    ##upload image to container
-
-    # get credentials
-    # ask user for rackspace credentials
-    username = raw_input('What is your username? ')
-    apiKey = raw_input('What is your api key? ')
-
-    # ask for imageid, region, and export container
-    #fileName = raw_input('Where is the file located? ')
-    container = raw_input('What is the name of the container? ')
-    region = raw_input('What region are you importing to? ')
-
-    pyrax.set_credentials(username, apiKey, region= region)
-
-    # create container to upload iamge
-    cf = pyrax.cloudfiles
-    cf.create_container(container)
-
-    pth = path to file
-    chksum = pyrax.utils.get_checksum(pth)
-    obj = cf.upload_file("example", pth, etag=chksum)
-    print "Calculated checksum:", chksum
-    print "Stored object etag:", obj.etag
 
 def import_img():
     # get credentials
@@ -76,11 +49,22 @@ def import_img():
     apiKey = raw_input('What is your api key? ')
     container = raw_input('What is the name of the container? ')
     region = raw_input('What region are you importing to? ')
+    vhd = raw_input("What is the file name? ")
 
     pyrax.set_credentials(username, apiKey, region= region)
 
+     # create container to upload iamge
+    cf = pyrax.cloudfiles
+    cf.create_container(container)
+
+    pth = os.getcwd() + "/" + vhd
+    chksum = pyrax.utils.get_checksum(pth)
+    obj = cf.upload_file(container, pth, etag=chksum)
+    print "Calculated checksum:", chksum
+    print "Stored object etag:", obj.etag
+
     imgs = pyrax.images
-    task = imgs.import_task(, container)
+    task = imgs.import_task(vhd, container)
 
     task.reload()
     while task.status == "processing":
@@ -88,8 +72,17 @@ def import_img():
        task.reload()
        print task.status
 
+print "Import or Export image?"
+print "1. Export"
+print "2. Import"
+choice = raw_input()
 
-#export_img()
+if choice == "1": 
+    export_img()
+else: 
+    import_img()
+
+
 #import_img()
 #download_vhd()
 #upload_vhd()
