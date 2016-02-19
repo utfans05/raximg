d38ae1c399b957ceb84dbfe351abdaf0112705fc
txhc4life <txhc4life@gmail.com>
2016-02-19 00:48:38 -0600

    Syncing what I worked on tonight.

--- raximg.py
+++ raximg.py
@@ -2,6 +2,19 @@ import requests
 import getpass
 import readline
 import time
+import json
+import argparse
+import logging
+logging.captureWarnings(True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("action", help="Actions include: import,export,download, status, upload or update")
+parser.add_argument("-u", "--username", help="Rackspace cloud account username.")
+parser.add_argument("-p", "--password", help="Rackspace cloud account password.")
+parser.add_argument("-i", "--image", help="Image to use for action.")
+parser.add_argument("-c", "--container", help="Container to use for action.")
+parser.add_argument("-r", "--region", help="Region of the image.")
+args = parser.parse_args()
 
 # request to authenticate and returns a tuple with token and account number
 def get_token(username,password):
@@ -9,8 +22,10 @@ def get_token(username,password):
     headers = {'Content-type': 'application/json'}
     payload = {'auth':{'passwordCredentials':{'username': username,'password': password}}}
     r = requests.post(url, headers=headers, json=payload)
-    print r.json()
-
+    data = r.json()
+    token = (data["access"]["token"]["id"])
+    account = (data["access"]["token"]["tenant"]["id"])
+    return token,account
 
 def create_container(token, account, region, container):
     if region == "dfw":
@@ -27,137 +42,100 @@ def create_container(token, account, region, container):
     headers = {"X-Auth-Token": token}
     r = requests.put(url, headers=headers)
 
-def export_img(token):
-    region = raw_input('region: ')
-    container = raw_input('container: ')
-    img_id = raw_input('image id: ')
-
+def export_img(token,account,region,container,image):
     create_container(token,account,region,container)
 
     url = "https://" + region +".images.api.rackspacecloud.com/v2/tasks"
     headers = {'Content-type': 'application/json', 'X-Auth-Token': token}
-    payload = {"type": "export","input":{"image_uuid": img_id,"receiving_swift_container": container}}
+    payload = {"type": "export","input":{"image_uuid": image,"receiving_swift_container": container}}
     r = requests.post(url, headers=headers, json=payload)
     data = r.json()
-    task_id = walk(data,'id')
-    print(task_id)
-    task_status(region, task_id)
+    task_id = data["id"]
+    task_status(token,region,task_id)
 
-def download_vhd(token, account):
-    region = raw_input("Region: ")
-    container = raw_input("Container: ")
-    vhd = raw_input("File: ")
-
+def download_img(token, account, region, image, container):
     if region == "dfw":
-        url = "https://storage101.dfw1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.dfw1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
     elif region == "iad":
-        url = "https://storage101.iad3.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.iad3.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
     elif region == "ord":
-        url = "https://storage101.ord1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.ord1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
     elif region == "syd":
-        url = "https://storage101.syd2.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.syd2.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
     elif region == "hkg":
-        url = "https://storage101.hkg1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.hkg1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
 
     headers = {"X-Auth-Token": token}
     r = requests.get(url,headers=headers, stream=True)
-    with open(vhd, 'wb') as f:
+    with open(image, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
                 #f.flush() commented by recommendation from J.F.Sebastian
-    return vhd
+    return image
 
-def upload_vhd(token, account):
-    region = raw_input("Region: ")
-    container = raw_input("Container: ")
-    vhd = raw_input("File: ")
-
+def upload_img(token, account,region, container, image):
     create_container(token,account,region,container)
 
     if region == "dfw":
-        url = "https://storage101.dfw1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.dfw1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
     elif region == "iad":
-        url = "https://storage101.iad3.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.iad3.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
     elif region == "ord":
-        url = "https://storage101.ord1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.ord1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
     elif region == "syd":
-        url = "https://storage101.syd2.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.syd2.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
     elif region == "hkg":
-        url = "https://storage101.hkg1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + vhd
+        url = "https://storage101.hkg1.clouddrive.com/v1/MossoCloudFS_" + account + "/" + container + "/" + image
 
     headers = {"X-Auth-Token": token, "Content-Type": "application/octet-stream"}
-    with open(vhd, 'rb') as f:
+    with open(image, 'rb') as f:
         r = requests.put(url,headers=headers,data=f)
 
-def import_img(token):
-    region = raw_input('region: ')
-    container = raw_input('container: ')
-    vhd = raw_input('image name: ')
-
+def import_img(token, image, container):
     url = "https://" + region +".images.api.rackspacecloud.com/v2/tasks"
     headers = {'Content-type': 'application/json', 'X-Auth-Token': token}
-    payload = {"type": "import","input":{"image_properties": {"name": vhd},"import_from": container + "/" + vhd}}
+    payload = {"type": "import","input":{"image_properties": {"name": image},"import_from": container + "/" + image}}
     r = requests.post(url, headers=headers, json=payload)
 
-def update_img(token):
-    region = raw_input('region: ')
-    img_id = raw_input('image id: ')
+    data = r.json()
+    task_id = data["id"]
+    task_status(token,region,task_id)
 
-    url = "https://" + region + ".images.api.rackspacecloud.com/v2/images/" + img_id
+def update_img(token,region,image):
+    url = "https://" + region + ".images.api.rackspacecloud.com/v2/images/" + imgage
     headers = {'Content-type': 'application/json', 'X-Auth-Token': token, 'Accept': 'application/openstack-images-v2.1-json-patch'}
     payload = {"op": "add", "path": "/vm-mode", "value": "hvm"}
     r = requests.patch(url,headers=headers, json=payload)
     data = r.json()
     print(data)
 
-def task_status(region, task_id):
+def task_status(token, region, task_id):
     url = "https://" + region +".images.api.rackspacecloud.com/v2/tasks" + "/" + task_id
     headers = {'Content-type': 'application/json', 'X-Auth-Token': token}
     r = requests.get(url, headers=headers)
-    data = r.text
-    print(data)
-    status = walk(data, 'status')
+    data = r.json()
+    status = data["status"]
     print(status)
     while(status != 'success'):
         r = requests.get(url, headers=headers)
-        data = r.text
-        status = walk(data,'status')
+        data = r.json()
+        status = data["status"]
         print(status)
         time.sleep(15)
+    if status == 'failure':
+        print("Something went wrong.")
     print(status)
 
-def menu():
-    print("Choose an action: ")
-    print("1. export")
-    print("2. import")
-    print("3. download")
-    print("4. upload")
-    action = raw_input()
+token,account = get_token(args.username,args.password)
 
-    if action == "1":
-        export_img(token)
-        menu()
-    elif action == "2":
-        import_img(token)
-        menu()
-    elif action == "3":
-        download_vhd(token, account)
-        menu()
-    elif action == "4":
-        upload_vhd(token,account)
-        menu()
-    else:
-        print("Try again")
-        menu()
-
-# start by asking for credentilas
-username = raw_input('username: ')
-password = getpass.getpass('password: ')
-
-# grab token and account info
-token = get_token(username,password)
-
-# ask the user what to do
-#menu()
-#update_img(token)
+if args.action == "export":
+    export_img(token, account, args.region,args.container, args.image)
+elif args.action == "import":
+    import_img(token, args.image, args.container)
+elif args.action == "download":
+    download_img(token, account, args.region, args.image, args.container)
+elif args.action == "upload":
+    upload_img(token, account, args.region, args.container, args.image)
+else:
+    print("bad input")
